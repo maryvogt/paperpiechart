6fcea90c8f920c5c2fcaf559cbb234ceee44d8b9
Mary Vogt <mary.vogt@comcast.net>
2012-02-12 10:34:03 -0500

    fixed coordination between js and paper - it actually draws a chart now

--- piechart.js
+++ piechart.js
@@ -27,8 +27,104 @@ function addPieChart(/*string*/ id, /*string*/ parentId,
     }; // end addPieChart
 
 
+
+// Obj is, uh, what? It's the div tag which contains the canvas for paper to draw the pie chart on
 function doDraw(/*object*/ obj)
 {
+    console.log("Entering draw method: obj is: "+obj.get(0)+ ", obj.data() is: "+(JSON.stringify(obj.data())));
+
+    if (obj.data("paths") != null) {
+        console.log("returning quickly from draw() because we already have paths for this data");
+        return true;
+    }
+
+    var nums = obj.data('numbers');
+    if (nums == null) {
+        console.log("no data for pie chart");
+        var text = new paper.PointText(paper.view.center);
+        text.fillColor = "black";
+        text.content = "No data for pie chart."
+    }
+    if (nums != null) {
+        console.log("we have numbers! "+nums);
+        // if we have numbers, we made sure it was an array when it was set in
+        var sum = 0;
+        for (var i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        var angles = new Array();
+        for (i = 0; i< nums.length; i++) {
+            angles[i] = (2 * Math.PI * nums[i] / sum);
+        }
+
+        // calculate center and radius for the pie chart
+        var center = paper.view.center;
+        console.log("doDraw(): paper.view.center is: "+paper.view.center.x+", "+paper.view.center.y);
+        var radius;
+        if (paper.view.size.width < paper.view.size.height) {
+            radius = paper.view.size.width / 2;
+        } else {
+            radius = paper.view.size.height / 2;
+        }
+
+        radius -= 5; // bring it in a little so the edge doesn't get cut off
+        console.log("doDraw(): radius is: "+radius);
+
+        // we are going to keep the Path objects around so that we can remove them
+        // from the Paper project if the data changes
+        var paths = new Array;
+
+        var start = new paper.Point(center.x + radius, center.y );      // first wedge starts at 3:00
+        var through;
+        var to;
+        var throughangle= 0;
+        var totalangle = 0;
+
+        for (i = 0; i < nums.length; i++, start = to) {
+            // each path starts at the center,
+            paths[i] = new paper.Path(center);
+            // goes straight out to the current startpoint,
+            paths[i].add(start);
+
+            // makes an arc along the perimeter of the circle
+            //    "through" is halfway along the circle (explain this better)
+            throughangle = totalangle + angles[i]/2;
+            totalangle = totalangle + angles[i];
+            var c1 = Math.cos(throughangle);
+            console.log("doDraw(): c1 = "+c1);
+
+            through = new paper.Point(radius * Math.cos(throughangle) + center.x, 
+                                      radius * Math.sin(throughangle) + center.y);
+
+
+            to = new paper.Point(radius * Math.cos(totalangle) + center.x,
+                                 radius * Math.sin(totalangle) + center.y);
+
+            console.log("doDraw(): paths["+i+"].arcTo(("+through.x+","+through.y+"), ("+to.x+","+ to.y+"))");
+            paths[i].arcTo(through, to);
+
+            // back to the center
+            paths[i].closePath();
+
+            // color it in
+            console.log("about to stroke the path with "+obj.data("strokecolor"));
+            if (obj.data("strokecolor") == null) {
+                obj.data("strokewidth", 4);
+                obj.data("strokecolor", "orange");
+            }
+            paths[i].strokeColor = obj.data("strokecolor");
+            paths[i].strokeWidth = obj.data("strokewidth");
+
+            // random colors for debugging
+            paths[i].fillColor = '#' + Math.floor(Math.random()*16777215).toString(16);    
+
+            $(this).data("paths", paths);
+
+        } // end for
+        paper.view.draw();
+    }     // end if nums != null
+
+
 }
 
 
@@ -60,6 +156,7 @@ function doDraw(/*object*/ obj)
 
             return this.each(function() {
                 obj = $(this);
+                //console.log("setting data on obj: "+obj.get(0));
                 obj.data(options); // this may be overriding previously set options to the defaults, check that
                 console.log("obj.data is now: "+JSON.stringify(obj.data()));
                    
@@ -93,7 +190,7 @@ function doDraw(/*object*/ obj)
 
                 paper.view.draw();
 
-                methods.draw();
+                doDraw(obj);
 
             }); // end function(), end this.each, end return
 
@@ -101,92 +198,7 @@ function doDraw(/*object*/ obj)
 
         draw: function (){  
              var obj = $(this);
-             console.log("Entering draw method: obj.data() is: "+(JSON.stringify(obj.data())));
-             
-             if (obj.data("paths") != null) {
-                 console.log("returning quickly from draw() because we already have paths for this data");
-                 return true;
-             }
-
-             var nums = obj.data('numbers');
-             if (nums == null) {
-                 console.log("no data for pie chart");
-                 var text = new paper.PointText(paper.view.center);
-                 text.fillColor = "black";
-                 text.content = "No data for pie chart."
-             }
-             if (nums != null) {
-                 console.log("we have numbers! "+nums);
-                 // if we have numbers, we made sure it was an array when it was set in
-                 var sum = 0;
-                 for (var i = 0; i < nums.length; i++) {
-                     sum += nums[i];
-                 }
-                 var angles = new Array();
-                 for (i = 0; i< nums.length; i++) {
-                     angles[i] = (2 * Math.PI * nums[i] / sum);
-                 }
-
-                 // calculate center and radius for the pie chart
-                 var center = paper.view.center;
-                 var radius;
-                 if (paper.view.size.width < paper.view.size.height) {
-                     radius = paper.view.size.width / 2;
-                 } else {
-                     radius = paper.view.size.height / 2;
-                 }
-                 
-                 radius -= 5; // bring it in a little so the edge doesn't get cut off
-
-                 // we are going to keep the Path objects around so that we can remove them
-                 // from the Paper project if the data changes
-                 var paths = new Array;
-
-                 var start = new paper.Point(center.x + radius, center.y );      // first wedge starts at 3:00
-                 var through;
-                 var to;
-                 var throughangle;
-                 var totalangle;
-
-                 for (i = 0; i < nums.length; i++, start = to) {
-                     // each path starts at the center,
-                     paths[i] = new paper.Path(center);
-                     // goes straight out to the current startpoint,
-                     paths[i].add(start);
-
-                     // makes an arc along the perimeter of the circle
-                     //    "through" is halfway along the circle (explain this better)
-                     throughangle = totalangle + angles[i]/2;
-                     totalangle = totalangle + angles[i];
-
-                     through = new paper.Point(radius * Math.cos(throughangle) + center.x, 
-                                               radius * Math.sin(throughangle) + center.y);
-
-
-                     to = new paper.Point(radius * Math.cos(totalangle) + center.x,
-                                          radius * Math.sin(totalangle) + center.y);
-
-                     paths[i].arcTo(through, to);
-
-                     // back to the center
-                     paths[i].closePath();
-
-                     // color it in
-                     console.log("about to stroke the path with "+$(this).data("strokecolor"));
-                     if (obj.data("strokecolor") == null) {
-                         obj.data("strokewidth", 4);
-                         obj.data("strokecolor", "orange");
-                     }
-                     paths[i].strokeColor = obj.data("strokecolor");
-                     paths[i].strokeWidth = obj.data("strokewidth");
-
-                     // random colors for debugging
-                     paths[i].fillColor = '#' + Math.floor(Math.random()*16777215).toString(16);    
-
-                     $(this).data("paths", paths);
-
-                 } // end for
-             }     // end if nums != null
+             doDraw(obj);
         }, // end draw
 
 
